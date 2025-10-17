4ebe8dd325d30615cdba486bff977c9663ee29a4
greatTinCole24 <54828133+greatTinCole24@users.noreply.github.com>
2025-10-16 21:30:27 -0400

    Pin next-sitemap to a published release

---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+export * from './types';
+export * from './semantic/registry';
+export * from './providers/types';
+export { ValorantProvider } from './providers/esports/valorant';
+export { NbaProvider } from './providers/sports/nba';
